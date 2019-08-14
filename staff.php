5aaf366ce93e5e935610cae1ee0c49a5dcf142aa
Bharath <bharathpalani0103@gmail.com>
2019-08-14 13:57:24 +0530

    index.php further editing & staff.php fixtures

--- staff.php
+++ staff.php
@@ -1,4 +1,39 @@
 <?php include ('header.php'); ?>
 <div class="alert alert-success">
   <strong>Need to Contact? </strong> Contact the staff &amp; General Enquiries <a class="alert-link" href="http://jalvirtual.org/en/index.php/contact"> here </a> .</div><h1>
-      <span class="label label-primary">Executive Staff </span></h1>
+      <span class="label label-primary">Executive Staff </span></h1><div class="panel panel-primary">
+          <div class="panel-heading">
+            <span class="label label-info">Touy Smith - Chief Executive Officer </span></div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-md-6">
+                &nbsp;</div>
+              <div class="col-md-6">
+                <span class="label label-info">Contact for: </span>
+                <p>
+                  &nbsp;</p>
+                <ul>
+                  <li>
+                    Website releated enquiries</li>
+                  <li>
+                    Contacts from company or organizations</li>
+                </ul>
+                <br>
+                <p>
+                  &nbsp;</p>
+              </div>
+            </div>
+            <table class="table">
+              <tbody>
+                <tr>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="panel-footer">
+            <span class="label label-info">Email: </span> <br>
+            <p>
+              <a href="mailto:admin@jalvirtual.org"> admin@jalvirtual.org </a></p>
+          </div>
+        </div>
+<?php include('footer.php'); ?>
